a570e7f28aa031d17205a3efaaba3d10aa331aa7
chlgr0 <89573280+chlgr0@users.noreply.github.com>
2021-08-26 18:00:29 +0900

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,41 @@
+$(document).ready(function(){
+    $(".slide-box").slick({});
+
+    $(".trend-wrap").slick({});
+
+    
+     $(".stage-wrap").slick({}); 
+
+
+     
+$('.item-wrap').slick({
+    centerMode: true,
+    centerPadding: '0px',
+    slidesToShow: 3,
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          arrows: true,
+          centerMode: true,
+          centerPadding: '40px',
+          slidesToShow: 3
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          arrows: true,
+          centerMode: true,
+          centerPadding: '40px',
+          slidesToShow: 1
+        }
+      }
+    ]
+  });
+
+
+});
+
+
+
